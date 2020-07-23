d7ee43c82e7647803b7a272f79ce064a53304314
Siddhant Doshi <siddhant.doshi@gmail.com>
2020-07-23 16:53:10 +0530

    Experiments with Hardy-Littlewood's 2nd conjecture and Fibonacci numbers

--- hardy_littlewood_conjecture2_test.py
+++ hardy_littlewood_conjecture2_test.py
@@ -1,6 +1,16 @@
+import math
 from prime_counting_function import prime_counter
 
-def check_conjecture(m, n):
+def check_conjecture1(m, n):
+	if prime_counter(m + n) <= prime_counter(m) + prime_counter(n):
+		return True
+
+	else:
+		return False
+
+def check_conjecture2(m, n):
+	prime_counter = lambda x: x / math.log(x)
+
 	if prime_counter(m + n) <= prime_counter(m) + prime_counter(n):
 		return True
 
